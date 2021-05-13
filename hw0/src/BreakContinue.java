eef02e3701749b33def5bde800fc76334d7379f9
Liang Liu <liuliang90@gmail.com>
2021-05-13 09:56:31 -0700

    Finished hw0

---
+++ hw0/src/BreakContinue.java
@@ -0,0 +1,27 @@
+public class BreakContinue {
+    public static void windowPosSum(int[] a, int n){
+        /* i is the position of the array */
+        for (int i=0; i<a.length; i++){
+            int sum = a[i];
+            if (sum < 0){
+                continue;
+            }
+            for (int j=1; j<=n; j++){
+                if (i + j > a.length-1){
+                    break;
+                }
+                sum = sum + a[i+j];
+            }
+            a[i] = sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a, n);
+
+        // Should print 4, 8, -3, 13, 9, 4
+        System.out.println(java.util.Arrays.toString(a));
+    }
+}
